2961915e47b67ac71acb31b07ededcd8995392e9
ashmit07 <54508463+ashmit07@users.noreply.github.com>
2019-11-07 17:59:44 +0530

    Add files via upload

---
+++ hpage.js
@@ -0,0 +1,18 @@
+document.getElementById("ls").addEventListener("click",function(){
+	window.location="registration.html";
+});
+document.getElementById("goHome").addEventListener("click",function(){
+	window.location="hpage.html";
+});
+document.getElementById("Mens wear").addEventListener('click',function(){
+	window.location="mens.html";
+});
+document.getElementById('Kids wear').addEventListener('click',function(){
+	window.location="kids.html";
+});
+document.getElementById('women wear').addEventListener('click',function(){
+	window.location="women.html";
+});
+document.getElementById('hd').addEventListener('click',function(){
+	window.location="hd.html";
+});
